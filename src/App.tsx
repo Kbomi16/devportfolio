9048451548d0700dda7b7692b132385dcde4fa79
김보미 <kimbomi2172@naver.com>
2024-07-05 14:26:48 +0900

    📦 chore: tailwind 폰트 및 색상 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>안녕하삼요</div>
+      <div className="text-2xl">안녕하삼요</div>
+      <div className="font-title">안녕하세요</div>
+      <h1 className="text-main font-bold">안녕하삼요</h1>
     </>
   )
 }
 
 export default App
-
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['HSSanTokki20-Regular'],
+      },
+      colors: {
+        primary: '#F4F2ED',
+        gray: {
+          50: '#7D7986',
+          40: '#A4A1AA',
+          30: '#CBC9CF',
+          20: '#E5E4E7',
+          10: '#F2F2F3',
+          5: '#FAFAFA',
+        },
+        green: {
+          20: '#6A7E60',
+          10: '#D4E4CF',
+        },
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
-
