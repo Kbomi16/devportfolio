22cd93cede31f2c292b2d88b1a6e35e56ae0fd50
김보미 <kimbomi2172@naver.com>
2024-07-11 19:34:51 +0900

    ✨ feat: 노션, 깃허브 연결

--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -2,8 +2,11 @@ import Header from '../components/Header'
 import greenBg from '../assets/imgs/greenBg.png.jpg'
 import ArrowUp from '../components/ArrowUp'
 import Footer from '../components/Footer'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { PROJECTS_DATA } from '../constants/PROJECTS_DATA'
+import githubImg from '../assets/icons/github.png'
+import notionImg from '../assets/icons/notion.png'
+import { motion } from 'framer-motion'
 
 export default function ProjectDetailPage() {
   const { id } = useParams()
@@ -20,15 +23,43 @@ export default function ProjectDetailPage() {
         className="min-h-fit bg-cover bg-center bg-no-repeat pt-12"
       >
         <Header />
-        <div className="base-container mb-10 bg-primary">
-          <div>
+        <div className="base-container bg-primary pb-10">
+          <div className="flex h-fit flex-col gap-4 py-4 md:flex-row">
             <img
               src={project.thumnail}
               alt={project.name}
-              className="h-auto w-full"
+              className="h-auto w-full rounded-xl border-2 border-black md:h-[22rem] md:w-[40rem]"
             />
-            <h1>{project.name}</h1>
-            <p>{project.description}</p>
+            <div className="flex flex-col md:items-stretch">
+              <div className="flex flex-col md:flex-1 md:gap-6">
+                <h2 className="pt-4 font-title text-xl font-bold md:text-[2rem]">
+                  {project.name}
+                </h2>
+                <p className="font-title md:text-[1rem]">
+                  {project.description}
+                </p>
+              </div>
+              <div className="flex gap-1 pt-4">
+                <Link to={project.github} target="_blank">
+                  <motion.img
+                    src={githubImg}
+                    alt=""
+                    className="h-12 w-12 cursor-pointer rounded-xl border-[1.5px] border-black bg-white p-1 md:h-20 md:w-20"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                  />
+                </Link>
+                <Link to={project.notion} target="_blank">
+                  <motion.img
+                    src={notionImg}
+                    alt=""
+                    className="h-12 w-12 cursor-pointer rounded-xl border-[1.5px] border-black bg-white p-1 md:h-20 md:w-20"
+                    whileHover={{ scale: 1.05 }}
+                    whileTap={{ scale: 0.95 }}
+                  />
+                </Link>
+              </div>
+            </div>
           </div>
         </div>
         <div className="fixed bottom-4 right-4 float-end flex">
