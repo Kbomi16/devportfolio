a78327c0389332475753d6095c1783790a0045c3
김보미 <kimbomi2172@naver.com>
2024-07-12 12:09:49 +0900

    💄 ui: 프로젝트 디테일 페이지 구현

--- src/constants/PROJECTS_DATA.ts
+++ src/constants/PROJECTS_DATA.ts
@@ -9,26 +9,35 @@ export const PROJECTS_DATA = [
     name: '급PAY',
     description:
       '급하게 일손이 필요한 자리에 더 많은 시급을 제공해서 아르바이트생을 구할 수 있는 서비스',
+    function: '',
+    stack: '',
+    contribution: '',
     github: 'https://github.com/902z/gheupPAY',
     notion:
       'https://marbled-pony-2be.notion.site/902z-5662a254ddaa44e9a5446bb315848cc1?pvs=4',
-    deploy: 'gheup-pay-project.vercel.app/',
+    deploy: 'https://gheup-pay-project.vercel.app/',
     thumnail: gheupPayImg,
   },
   {
     id: 2,
     name: 'i-Konnect',
     description: '추억의 아이돌 조공 후원 서비스',
+    function: '',
+    stack: '',
+    contribution: '',
     github: 'https://github.com/BestSprinters/i-Konnect',
     notion:
       'https://saber-lip-4c8.notion.site/15-i-Konnect-5afb70e2636e48c1a83f2c9790dacb6c?pvs=4',
-    deploy: 'i-konnect.netlify.app',
+    deploy: 'https://i-konnect.netlify.app',
     thumnail: iKonnectImg,
   },
   {
     id: 3,
     name: 'fumease',
     description: '사용자 맞춤 향수 추천 쇼핑몰 웹사이트',
+    function: '',
+    stack: '',
+    contribution: '',
     github: 'https://github.com/902z/gheupPAY',
     notion:
       'https://saber-lip-4c8.notion.site/Fumease-76d9a29fa5344fdca68c434ee19d2fc6?pvs=4',
@@ -39,6 +48,9 @@ export const PROJECTS_DATA = [
     id: 4,
     name: 'devportfolio',
     description: '포트폴리오',
+    function: '',
+    stack: '',
+    contribution: '',
     github: 'https://github.com/Kbomi16/devportfolio',
     notion:
       'https://saber-lip-4c8.notion.site/devportfolio-4b6ed986c550482faeb93fc849417083?pvs=4',
--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -7,6 +7,8 @@ import { PROJECTS_DATA } from '../constants/PROJECTS_DATA'
 import githubImg from '../assets/icons/github.png'
 import notionImg from '../assets/icons/notion.png'
 import { motion } from 'framer-motion'
+import deploy from '../assets/icons/deploy.svg'
+import download from '../assets/icons/download.svg'
 
 export default function ProjectDetailPage() {
   const { id } = useParams()
@@ -24,11 +26,11 @@ export default function ProjectDetailPage() {
       >
         <Header />
         <div className="base-container bg-primary pb-10">
-          <div className="flex h-fit flex-col gap-4 py-4 md:flex-row">
+          <div className="flex h-fit flex-col gap-4 py-4 lg:flex-row">
             <img
               src={project.thumnail}
               alt={project.name}
-              className="h-auto w-full rounded-xl border-2 border-black md:h-[22rem] md:w-[40rem]"
+              className="h-auto w-full rounded-xl border-2 border-black lg:h-[22rem] lg:w-[40rem]"
             />
             <div className="flex flex-col md:items-stretch">
               <div className="flex flex-col md:flex-1 md:gap-6">
@@ -39,27 +41,73 @@ export default function ProjectDetailPage() {
                   {project.description}
                 </p>
               </div>
-              <div className="flex gap-1 pt-4">
-                <Link to={project.github} target="_blank">
-                  <motion.img
-                    src={githubImg}
-                    alt=""
-                    className="h-12 w-12 cursor-pointer rounded-xl border-[1.5px] border-black bg-white p-1 md:h-20 md:w-20"
+              <div className="flex items-end justify-between gap-1 pt-4">
+                <div className="flex gap-1">
+                  <Link to={project.github} target="_blank">
+                    <motion.img
+                      src={githubImg}
+                      alt=""
+                      className="h-12 w-12 cursor-pointer rounded-xl border-[1.5px] border-black bg-white p-1 md:h-20 md:w-20"
+                      whileHover={{ scale: 1.05 }}
+                      whileTap={{ scale: 0.95 }}
+                    />
+                  </Link>
+                  <Link to={project.notion} target="_blank">
+                    <motion.img
+                      src={notionImg}
+                      alt=""
+                      className="h-12 w-12 cursor-pointer rounded-xl border-[1.5px] border-black bg-white p-1 md:h-20 md:w-20"
+                      whileHover={{ scale: 1.05 }}
+                      whileTap={{ scale: 0.95 }}
+                    />
+                  </Link>
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Link to={project.deploy} target="_blank">
+                    <motion.button
+                      className="flex w-[8rem] items-center justify-center gap-1 rounded-full border bg-white py-1 shadow-md transition-all"
+                      whileHover={{ scale: 1.05 }}
+                      whileTap={{ scale: 0.95 }}
+                    >
+                      DEPLOY <img src={deploy} alt="" className="h-5 w-5" />
+                    </motion.button>
+                  </Link>
+                  <motion.button
+                    className="flex w-[8rem] items-center justify-center gap-1 rounded-full border bg-white py-1 shadow-md transition-all"
                     whileHover={{ scale: 1.05 }}
                     whileTap={{ scale: 0.95 }}
-                  />
-                </Link>
-                <Link to={project.notion} target="_blank">
-                  <motion.img
-                    src={notionImg}
-                    alt=""
-                    className="h-12 w-12 cursor-pointer rounded-xl border-[1.5px] border-black bg-white p-1 md:h-20 md:w-20"
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }}
-                  />
-                </Link>
+                  >
+                    PDF <img src={download} alt="" className="h-6 w-6" />
+                  </motion.button>
+                </div>
               </div>
             </div>
+          </div>
+
+          <hr className="my-4 h-0.5 border bg-black md:my-6" />
+          <div>
+            <h2 className="relative inline-block font-title text-2xl">
+              💡주제
+            </h2>
+            <p>{project.description}</p>
+          </div>
+          <div>
+            <h2 className="relative inline-block font-title text-2xl">
+              ✨주요 기능
+            </h2>
+            <p>{project.description}</p>
+          </div>
+          <div>
+            <h2 className="relative inline-block font-title text-2xl">
+              🛠️기술 스택
+            </h2>
+            <p>{project.description}</p>
+          </div>
+          <div>
+            <h2 className="relative inline-block font-title text-2xl">
+              👧🏻기여도
+            </h2>
+            <p>{project.description}</p>
           </div>
         </div>
         <div className="fixed bottom-4 right-4 float-end flex">
