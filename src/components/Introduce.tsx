d5831b847e195d5e93642cf00f0f8bf500c197ce
김보미 <kimbomi2172@naver.com>
2025-03-28 11:44:39 +0900

    ♻️ refactor: introduce 스크롤카드로 변경

--- src/components/Introduce.tsx
+++ src/components/Introduce.tsx
@@ -1,230 +1,99 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { motion } from 'framer-motion'
-import {
-  TEXT_UNDERLINE_VARIANTS,
-  TEXT_VARIANTS,
-} from '../constants/TEXT_VARIANTS'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect, useRef, useState } from 'react'
-import { Link } from 'react-router-dom'
-import UnderlineAnimation from './UnderlineAnimation'
+import { useEffect, useState } from 'react'
+import { FaBook, FaClipboardList, FaLaptopCode, FaUsers } from 'react-icons/fa'
+import { TEXT_VARIANTS } from '../constants/TEXT_VARIANTS'
 import Light from './Light'
-import todayMenu from '../assets/files/todayMenu.pdf'
-import toytie from '../assets/files/toytie.pdf'
-import fumease from '../assets/files/fumease.pdf'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Mousewheel } from 'swiper/modules'
-import 'swiper/css'
-import ScrollIndicator from './ScrollIndicator'
+
+const cardData = [
+  {
+    icon: <FaBook size={40} />,
+    title: '프론트엔드 개발자 김보미입니다.',
+    text: '사용자 경험을 중요시하며, 문제 해결 능력과 협업 능력을 갖추었습니다.',
+  },
+  {
+    icon: <FaUsers size={40} />,
+    title: '사용자 경험',
+    text: '사용자 입장에서 느낀 불편함을 깊이 이해하고 이를 바탕으로 효과적인 프로젝트 아이디어를 제시합니다.',
+  },
+  {
+    icon: <FaLaptopCode size={40} />,
+    title: '애자일 스크럼 방법론 활용',
+    text: '애자일 사상과 스크럼 방법론을 적극적으로 활용하여 효율적이고 체계적인 프로젝트 관리를 실현합니다.',
+  },
+  {
+    icon: <FaBook size={40} />,
+    title: '블로그 운영',
+    text: '70개 이상의 글이 작성된 기술 블로그를 운영하며, 배운 지식을 적극적으로 공유하고 있습니다.',
+  },
+  {
+    icon: <FaClipboardList size={40} />,
+    title: '프로젝트 관리 도구 사용',
+    text: 'Jira, Notion 등의 도구를 활용하여 프로젝트 계획 및 관리에 능숙하며, 업무 효율성을 극대화합니다.',
+  },
+]
 
 export default function Introduce() {
-  const [hoverIndex, setHoverIndex] = useState<number | null>(null)
-  const [showScrollIndicator, setShowScrollIndicator] = useState<boolean>(true)
-
-  const firstContentRef = useRef<HTMLDivElement | null>(null)
-  const lastContentRef = useRef<HTMLDivElement | null>(null)
-
-  const handleMouseEnter = (index: number) => {
-    setHoverIndex(index)
-  }
-
-  const handleMouseLeave = () => {
-    setHoverIndex(null)
-  }
+  const [offset, setOffset] = useState(0)
 
   useEffect(() => {
-    AOS.init({ duration: 800 })
+    const interval = setInterval(() => {
+      setOffset((prev) => prev + 1)
+    }, 3000)
+
+    return () => clearInterval(interval)
   }, [])
 
-  const projects = [
-    {
-      name: '질문 선택을 통한 메뉴 추천 앱',
-      pdfUrl: todayMenu,
-      textColor: 'text-yellow-400',
-    },
-    {
-      name: '토이 프로젝트 팀원 매칭 웹사이트',
-      pdfUrl: toytie,
-      textColor: 'text-amber-700',
-    },
-    {
-      name: 'AI를 활용한 향수 쇼핑 웹사이트',
-      pdfUrl: fumease,
-      textColor: 'text-black',
-    },
-  ]
-
-  const handleSlideChange = (swiper: any) => {
-    setShowScrollIndicator(!swiper.isEnd) // 마지막 슬라이드가 아닐 때 스크롤 유도 표시
-    if (swiper.isEnd) {
-      lastContentRef.current?.scrollIntoView({ behavior: 'smooth' })
-    } else if (swiper.isBeginning) {
-      firstContentRef.current?.scrollIntoView({ behavior: 'smooth' })
-    }
-  }
-
   return (
-    <div className="bg-primary p-5 text-black opacity-80 md:p-20">
-      <div className="rounded-xl border-2 border-black p-4">
-        <Light />
-        <motion.div initial="hidden" animate="visible" variants={TEXT_VARIANTS}>
-          <h2 className="relative inline-block text-2xl font-bold md:text-[2rem]">
-            배움엔 끝이 없다.
-            <br />
-            끊임없는 배움과 도전을 즐기며 성장하고 있는{' '}
-            <p className="inline-block text-4xl text-red-300">김보미</p>
-            입니다.
-            <motion.svg
-              className="absolute bottom-0 left-0"
-              width="100%"
-              height="10px"
-              viewBox="0 0 800 10"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <motion.line
-                x1="0"
-                y1="5"
-                x2="800"
-                y2="5"
-                stroke="#000000"
-                strokeWidth="2"
-                initial="hidden"
-                animate="visible"
-                variants={TEXT_UNDERLINE_VARIANTS}
-              />
-            </motion.svg>
-          </h2>
-        </motion.div>
-      </div>
-      <br />
-      <br />
-      <br />
+    <div className="p-10">
+      <div className="mx-auto max-w-[1200px] overflow-hidden">
+        <div className="mb-20 rounded-xl bg-primary bg-opacity-60 p-8">
+          <Light />
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={TEXT_VARIANTS}
+          >
+            <h2 className="relative inline-block text-2xl font-bold text-black md:text-[2rem]">
+              배움엔 끝이 없다.
+              <br />
+              끊임없는 배움과 도전을 즐기며 성장하고 있는{' '}
+              <p className="inline-block text-5xl font-bold text-white">
+                김보미
+              </p>
+              입니다.
+            </h2>
+          </motion.div>
+        </div>
 
-      <div className="z-10 flex flex-col gap-8">
-        <Swiper
-          direction="vertical"
-          modules={[Mousewheel]}
-          mousewheel={{ forceToAxis: true, invert: false }}
-          slidesPerView={1}
-          autoHeight={true}
-          className="h-[calc(100vh-30rem)] overflow-y-auto"
-          onSlideChange={handleSlideChange}
-        >
-          <SwiperSlide>
-            <div ref={firstContentRef}>
-              <UnderlineAnimation>1. Introduce.</UnderlineAnimation>
-              <p className="whitespace-normal md:text-xl">
-                안녕하세요! <strong>프론트엔드 개발자 김보미입니다.</strong>
-                <br />
-                <br />
-                사용자 입장에서 느낀 불편함을 그냥 넘기지 않고 깊이 이해하는
-                습관을 통해 프로젝트 아이디어를 도출하고 있습니다. <br />
-                <br />
-                체계적으로 계획을 세워 프로젝트를 진행하며, 웹 기술을 활용하여
-                직관적이고 미적인 사용자 경험을 만들어 내도록 노력하고 있습니다.
-                뛰어난 문제 해결 능력과 협업 능력을 갖추어 프로젝트를 성공적으로
-                완성하고 있습니다.
-              </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              <UnderlineAnimation>
-                2. 일의 우선순위를 정하고, 계획하는 것을 좋아합니다.
-              </UnderlineAnimation>
-              <p className="whitespace-normal md:text-xl">
-                저는 복잡한 상황에서도 해야 할 일들을 체계적으로 정리하고 조율할
-                수 있습니다. 중요한 일과 중요하지 않은 일을 구분하며 앞으로 해야
-                할 일들을 계획적으로 준비합니다.
-                <br />
-                <br />
-                애자일 사상에 따른 스크럼 방법론을 적극 활용해 보다 체계적으로
-                프로젝트를 관리하고, 마감 계획을 이행하는 것을 중요시합니다.
-              </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div>
-              <UnderlineAnimation>
-                3. 배움을 나누며 기록하는 것을 좋아합니다.
-              </UnderlineAnimation>
-              <p className="whitespace-normal md:text-xl">
-                저는{' '}
-                <Link
-                  to="https://github.com/Study-FE-Techbook/Modern-React-Deep-Dive"
-                  target="_blank"
-                  className="text-xl font-bold text-red-400 transition-all duration-700 hover:text-2xl hover:underline"
-                >
-                  스터디 활동
-                </Link>
-                에 적극 참여했고, 기술 블로그를 운영하며{' '}
-                <Link
-                  to="https://bori-note.tistory.com"
-                  target="_blank"
-                  className="text-xl font-bold text-purple-400 transition-all duration-700 hover:text-2xl hover:underline"
-                >
-                  50개 이상의 글
-                </Link>
-                을 작성했습니다. <br />
-                <br />
-                이를 통해 제가 배운 지식을 정리하고, 다른 사람들과 나누는 데 큰
-                기쁨을 느끼고 있습니다. 이러한 기록은 제게 지속적인 학습의
-                원동력이 되고 있습니다.
-              </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div ref={lastContentRef}>
-              <UnderlineAnimation>
-                4. 사용자 경험을 중시합니다.
-              </UnderlineAnimation>
-              <p className="whitespace-normal md:text-xl">
-                사용자 입장에서 느끼는 불편함을 간과하지 않고 깊이 이해하는
-                습관을 통해 다양한 프로젝트 아이디어를 도출하고 있습니다.
-                <br />
-                <br />
-                사용자의 목소리에 귀 기울이며, 그들의 필요와 문제를 해결하기
-                위한 해결책을 고민하는 과정에서 더욱 창의적인 아이디어를
-                발전시킬 수 있었습니다. 사용자의 불편함을 해소하기 위한 다양한
-                프로젝트를 수행했습니다.
-                <br />
-                <br />
-                {projects.map((project, index) => (
-                  <div key={index} className="relative inline-block">
-                    <p
-                      className={`cursor-pointer text-xl font-bold transition-all duration-700 hover:text-2xl ${project.textColor}`}
-                      onMouseEnter={() => handleMouseEnter(index)}
-                      onMouseLeave={handleMouseLeave}
-                    >
-                      <Link
-                        to={project.pdfUrl}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {project.name},
-                      </Link>{' '}
-                    </p>
-                    {hoverIndex === index && (
-                      <div
-                        className={`${project.textColor} no-opacity absolute left-0 z-30 w-fit rounded bg-primary p-2 text-sm transition-all duration-700`}
-                        onMouseEnter={() => handleMouseEnter(index)}
-                        onMouseLeave={handleMouseLeave}
-                      >
-                        텍스트 클릭 시 PDF를 볼 수 있어요.
-                      </div>
-                    )}
+        {/* 🔄 무한 스크롤 효과 추가 */}
+        <div className="relative flex w-full overflow-hidden">
+          <motion.div
+            className="flex gap-6"
+            animate={{ x: `-${(offset % cardData.length) * 300}px` }} // 카드 크기 늘리기 위해 320px로 조정
+            transition={{ ease: 'linear', duration: 0.8 }}
+            style={{ width: `${cardData.length * 380}px` }} // 전체 크기 맞추기
+          >
+            {cardData.map((card, index) => (
+              <div
+                key={index}
+                className="min-w-[380px] rounded-xl border-2 border-primary bg-black px-6 py-10 text-black shadow-lg"
+              >
+                <div className="flex items-center justify-center gap-2">
+                  <div className="flex justify-center text-primary">
+                    {card.icon}
                   </div>
-                ))}
-                등을 개발하며, 사용자 중심의 개발 방법과 문제해결 능력을
-                키웠습니다. 이러한 프로젝트 과정에서 여러 문제를 해결하며,
-                사용자 경험을 개선하는 데에 집중했습니다.
-              </p>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+                  <h3 className="text-center text-lg font-bold text-primary">
+                    {card.title}
+                  </h3>
+                </div>
+                <p className="mt-4 px-8 text-center text-gray-400">
+                  {card.text}
+                </p>
+              </div>
+            ))}
+          </motion.div>
+        </div>
       </div>
-      {showScrollIndicator && <ScrollIndicator />}
     </div>
   )
 }
